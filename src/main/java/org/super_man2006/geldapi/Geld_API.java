913f09beaedec85b69327fe98edfca08bdaa265b
Senne98 <94780127+Senne98@users.noreply.github.com>
2023-05-16 22:19:10 +0200

    Update Geld_API.java

--- src/main/java/org/super_man2006/geldapi/Geld_API.java
+++ src/main/java/org/super_man2006/geldapi/Geld_API.java
@@ -4,7 +4,6 @@ import net.kyori.adventure.text.Component;
 import net.kyori.adventure.text.format.NamedTextColor;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.super_man2006.geldapi.commands.Balance;
-import org.super_man2006.geldapi.commands.Test;
 import org.super_man2006.geldapi.json.Currency;
 
 import java.io.File;
